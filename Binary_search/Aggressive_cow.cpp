318dda9501771e5716be6921a1014e23bd1c3a91
Aakashsaini03 <aakashrohtashsaini74@gmail.com>
2025-08-30 13:48:04 +0530

    Added Aggressive Cow solution in Binary_search

--- Binary_search/Aggressive_cow.cpp
+++ Binary_search/Aggressive_cow.cpp
@@ -1,1 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Possible(vector<int>&arr,int mid,int k)
+{
+    int ans=0;
+    int prev=arr[0];
+    int cnt=1;
+    for(int i=1;i<arr.size();i++)
+    {
+        if(arr[i]-prev>=mid)
+        {
+            cnt++;
+            prev=arr[i];
+        }
+    }
+    return cnt>=k;
+}
+int aggressiveCows(vector<int>&arr,int k)
+{
+    int low=1;
+    int ans=0;
+    int high=*max_element(arr.begin(),arr.end());
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(Possible(arr,mid,k))
+        {
+            ans=mid;
+            low=mid+1;
+        }
+        else
+          high=mid-1;
+          
+    }
+    return ans;
+
+}
+int main()
+{
+
+        int n;
+        cin>>n;
+    vector<int>stalls(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>stalls[i];
+    } 
+    int k;
+    cin>>k;
+    int ans = aggressiveCows(stalls, k);
+    cout<<"the maximim distance to manage the aggressive cows:\n"<<endl;
+    cout << ans;
+    return 0;
+}
 
